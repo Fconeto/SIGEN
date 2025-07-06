576a8904591c2a8581c4cb88c46eb1d5b7dd8a6c
Francisco Linhares <linharesipuce@gmail.com>
2025-07-06 14:34:47 -0300

    Alteração na procedure para retornar a entidade nula e não os campos

--- SIGEN.Infrastructure/Scripts/GetPendingSearchByResidenciaId.sql
+++ SIGEN.Infrastructure/Scripts/GetPendingSearchByResidenciaId.sql
@@ -26,18 +26,4 @@ BEGIN
         FROM Residencia
         WHERE ResidenciaId = @ResidenciaId
     END
-    ELSE
-    BEGIN
-        SELECT 
-            CAST(NULL AS BIGINT) AS Id,
-            CAST(NULL AS BIGINT) AS CodigoDaLocalidade,
-            CAST(NULL AS INT) AS TipoDoImovel,
-            CAST(NULL AS INT) AS Demolida,
-            CAST(NULL AS INT) AS Numero,
-            CAST(NULL AS NVARCHAR(255)) AS Complemento,
-            CAST(NULL AS BIGINT) AS NumeroDoQuarteirao,
-            CAST(NULL AS NVARCHAR(255)) AS ComplementoDoQuarteirao,
-            CAST(NULL AS NVARCHAR(255)) AS NomeDoMorador,
-            CAST(NULL AS INT) AS Inabitado
-    END
 END
