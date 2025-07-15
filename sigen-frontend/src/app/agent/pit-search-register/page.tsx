86fbfb48f2b2f3d189f9477dfe6588774d4b6a20
Natan Sales <victornatansalles@gmail.com>
2025-07-15 20:03:36 -0300

    criação da tela para redirecionamento do cadastro de pesquisa de PIT

---
+++ sigen-frontend/src/app/agent/pit-search-register/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import SearchRegisterPITForm from "@/app_features/search/pit-search-register/pit-search-register";
+
+export default function SearchRegisterPITFormPage() {
+  return <SearchRegisterPITForm></SearchRegisterPITForm>;
+}
